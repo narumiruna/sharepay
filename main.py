4ca7d787b93ea2894356308d8c0fc11b0a0f9678
narumi <4680567+narumiruna@users.noreply.github.com>
2024-02-03 22:01:47 +0800

    refactor

--- main.py
+++ main.py
@@ -7,36 +7,25 @@ from sharepay import Project
 def main() -> None:
     members = Member.from_names(["narumi", "dogiko", "ben", "john"])
 
-    project = Project(name="test", members=list(members.values()))
+    project = Project(name="test", currency=Currency.JPY)
 
-    project.add_payment(
+    payments = [
         Payment(
             amount=300,
             currency=Currency.JPY,
             payer=members["narumi"],
             members=[members["narumi"], members["dogiko"], members["ben"]],
-        )
-    )
-    project.add_payment(
+        ),
         Payment(
             amount=900,
             currency=Currency.JPY,
             payer=members["dogiko"],
             members=[members["narumi"], members["dogiko"], members["ben"]],
-        )
-    )
-    project.add_payment(
-        Payment(amount=600, currency=Currency.JPY, payer=members["ben"], members=[members["john"], members["john"]])
-    )
+        ),
+        Payment(amount=600, currency=Currency.JPY, payer=members["ben"], members=[members["john"], members["john"]]),
+    ]
 
-    for d in project.debts:
-        print(d)
-
-    x = 0
-    for _, m in members.items():
-        x += m.balance
-        print(m)
-    assert x == 0
+    project.add_payments(payments)
 
     project.settle_up()
 
--- sharepay/project.py
+++ sharepay/project.py
@@ -14,17 +14,31 @@ from .rate import query_rate
 
 class Project(BaseModel):
     name: str
-    members: list[Member]
+    members: dict[str, Member] = Field(default_factory=dict)
     currency: Currency = Field(default=Currency.TWD)
-    payments: list[Payment] = Field(default=[])
-    debts: list[Debt] = Field(default=[])
+    payments: list[Payment] = Field(default_factory=list)
+    debts: list[Debt] = Field(default_factory=list)
+
+    def add_member(self, member: Member) -> Project:
+        if member.name not in self.members:
+            self.members[member.name] = member
+        return self
 
     def add_payment(self, payment: Payment) -> Project:
+        self.add_member(payment.payer)
+        for m in payment.members:
+            self.add_member(m)
+
         self.payments.append(payment)
         self.debts += payment.debts()
 
+    def add_payments(self, payments: list[Payment]) -> Project:
+        for payment in payments:
+            self.add_payment(payment)
+        return self
+
     def reset_balance(self) -> None:
-        for m in self.members:
+        for m in self.members.values():
             m.balance = 0
 
     def cal_balance(self) -> None:
@@ -41,7 +55,7 @@ class Project(BaseModel):
         self.reset_balance()
         self.cal_balance()
 
-        members = copy.deepcopy(self.members)
+        members = copy.deepcopy(list(self.members.values()))
         while len(members) > 1:
             members = sorted(members, key=lambda x: -x.balance)
 
