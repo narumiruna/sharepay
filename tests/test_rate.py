029fc7166edd98ce015a77101badcbd53f9ef635
narumi <4680567+narumiruna@users.noreply.github.com>
2024-02-04 12:22:35 +0800

    test rate

---
+++ tests/test_rate.py
@@ -0,0 +1,7 @@
+from sharepay.rate import query_rate
+
+
+def test_query_rate():
+    assert query_rate("JPY", "TWD") > 0
+    assert query_rate("TWD", "JPY") > 0
+    assert query_rate("TWD", "TWD") == 1
