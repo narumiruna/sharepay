a428ad6f601b7c72475bd52b852d7172ddc6d67c
narumi <4680567+narumiruna@users.noreply.github.com>
2024-02-04 21:36:08 +0800

    rename

--- example.py
+++ example.py
@@ -6,8 +6,8 @@ def main() -> None:
     project = SharePay(name="Sendai", currency=Currency.JPY, alias={"yoan": "john"})
 
     project.add_payment(amount=300, currency="JPY", payer_name="narumi", member_names=["narumi", "dogiko", "ben"])
-    project.add_payment(amount=900, currency="JPY", payer_name="dogiko", member_names=["narumi", "dogiko", "ben"])
-    project.add_payment(amount=600, currency="JPY", payer_name="ben", member_names=["john", "cindy"])
+    project.add_payment(amount=600, currency="JPY", payer_name="dogiko", member_names=["dogiko", "ben", "john"])
+    project.add_payment(amount=900, currency="JPY", payer_name="ben", member_names=["john", "yoan"])
 
     project.settle_up()
 
