30eb813274d0c21ae91c7ef400526d09804a7c67
narumi <4680567+narumiruna@users.noreply.github.com>
2024-02-05 19:24:42 +0800

    add test_payment_validate_time

--- tests/test_payment.py
+++ tests/test_payment.py
@@ -5,14 +5,14 @@ from sharepay.member import Member
 from sharepay.payment import Payment
 
 
-def test_payment_init() -> None:
-    a = Member(name="a")
-    b = Member(name="b")
-    c = Member(name="c")
-    p = Payment(amount=300, currency=Currency.TWD, payer=a, members=[a, b, c], time="2024-01-01")
+def test_payment_validate_time() -> None:
+    a = 100
+    c = Currency.TWD
+    m = Member(name="test")
+    dt = datetime(year=2024, month=1, day=1)
 
-    assert p.amount == 300
-    assert p.currency == Currency.TWD
-    assert p.payer == a
-    assert p.members == [a, b, c]
-    assert p.time == datetime(year=2024, month=1, day=1)
+    p = Payment(amount=a, currency=c, payer=m, members=[], time=dt.strftime("%Y-%m-%d"))
+    assert p.time == dt
+
+    p = Payment(amount=a, currency=c, payer=m, members=[], time=dt)
+    assert p.time == dt
