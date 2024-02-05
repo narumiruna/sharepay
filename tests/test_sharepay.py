9fbfca76bfab24a7006fc5152f2ffb3491eae097
narumi <4680567+narumiruna@users.noreply.github.com>
2024-02-05 17:49:36 +0800

    test settle_up

--- tests/test_sharepay.py
+++ tests/test_sharepay.py
@@ -2,7 +2,7 @@ from sharepay.currency import Currency
 from sharepay.sharepay import SharePay
 
 
-def test_sharepay():
+def test_sharepay_balance() -> None:
     s = SharePay(name="test")
     s.add_payment(amount=300, payer_name="a", member_names=["a", "b", "c"], currency=Currency.TWD)
     s.add_payment(amount=200, payer_name="b", member_names=["b", "c"], currency=Currency.TWD)
@@ -11,3 +11,15 @@ def test_sharepay():
     assert s.members["a"].balance == 300 * 2 / 3
     assert s.members["b"].balance == -300 / 3 + 200 / 2
     assert s.members["c"].balance == -300 / 3 - 200 / 2
+
+
+def test_sharepay_settle_up() -> None:
+    s = SharePay(name="test")
+    s.add_payment(amount=300, payer_name="a", member_names=["a", "b", "c"], currency=Currency.TWD)
+    s.add_payment(amount=200, payer_name="b", member_names=["b", "c"], currency=Currency.TWD)
+    transactions = s.settle_up()
+
+    assert len(transactions) == 1
+    assert transactions[0].sender.name == "c"
+    assert transactions[0].recipient.name == "a"
+    assert transactions[0].amount == 200
