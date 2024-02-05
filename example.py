5b879560fb6a00f79e4acdd8c6159a1dbb7efa7e
narumi <4680567+narumiruna@users.noreply.github.com>
2024-02-05 17:14:06 +0800

    update example

--- example.py
+++ example.py
@@ -3,13 +3,13 @@ from sharepay import SharePay
 
 
 def main() -> None:
-    project = SharePay(name="Sendai", currency=Currency.TWD, alias={"yoan": "john"})
+    p = SharePay(name="Sendai", currency=Currency.TWD, alias={"yoan": "john"})
 
-    project.add_payment(amount=300, currency="JPY", payer_name="narumi", member_names=["narumi", "dogiko", "ben"])
-    project.add_payment(amount=600, currency="JPY", payer_name="dogiko", member_names=["dogiko", "ben", "john"])
-    project.add_payment(amount=900, currency="JPY", payer_name="ben", member_names=["john", "yoan"])
+    p.add_payment(amount=300, currency="JPY", payer_name="narumi", member_names=["narumi", "dogiko", "ben"])
+    p.add_payment(amount=600, currency="JPY", payer_name="dogiko", member_names=["dogiko", "ben", "john"])
+    p.add_payment(amount=900, currency="JPY", payer_name="ben", member_names=["john", "yoan"])
 
-    project.settle_up()
+    p.settle_up()
 
 
 if __name__ == "__main__":
